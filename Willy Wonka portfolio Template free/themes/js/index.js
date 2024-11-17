a2d429e9b855bf76c8d4798392c0a93b0b3c7e8c
cohen <yair.cohen@ortmontreuil.fr>
2024-11-17 19:02:35 +0100

    projet

--- Willy Wonka portfolio Template free/themes/js/index.js
+++ Willy Wonka portfolio Template free/themes/js/index.js
@@ -143,6 +143,7 @@ gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#blog',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
 })
+
 $('#home-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
